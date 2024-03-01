d9415e1ccee714c8dc7b854a5a9620219194e1f9
Bill Levien <blevien@users.noreply.github.com>
2024-03-01 06:09:07 +0000

    Check50 Tests for submissions

---
+++ PT1/__init__.py
@@ -0,0 +1,8 @@
+import check50
+
+@check50.check()
+def exists():
+    """fixme2, weather & homework exists"""
+    check50.exists("fixme2.py")
+    check50.exists("weather.py")
+    check50.exists("homework.py")
