7ff6847af1158dafb937b3ca4ab1130adc6c5208
Bill Levien <blevien@users.noreply.github.com>
2024-02-29 20:39:13 -1000

    Update fixme2.py

--- PT1/fixme2.py
+++ PT1/fixme2.py
@@ -40,14 +40,14 @@ print()
 print("------ Q7 ------")
 x = input("What's x? ")
 y = input("What's y? ")
-print("TODO")
+print(x + y)
 print()
 
-# Task 8: Improve the calculator program to handle floating-point numbers.
+# Task 8: Improve the calculator program to handle floating-point (decimal) numbers.
 print("------ Q8 ------")
 x = input("What's x? ")
 y = input("What's y? ")
-print(TODO)
+print(x + y)
 print()
 
 # Task 9: Given a sentence with multiple occurrences of the word "apple" and "banana"
