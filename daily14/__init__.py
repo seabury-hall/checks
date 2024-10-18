0b3e1f06ae739dfb74e8e9972c3482dfd3a12fa7
Bill Levien <blevien@users.noreply.github.com>
2024-10-18 20:16:01 +0000

    Remove "comile" requirement

--- daily14/__init__.py
+++ daily14/__init__.py
@@ -6,8 +6,3 @@ class Prime(Checks):
 	def exists(self):
 		"""daily14.c exists"""
 		self.require("daily14.c")
-
-	@check("exists")
-	def compiles(self):
-		"""daily14.c compiles"""
-		self.spawn("clang -o daily14 daily14.c -lcs50 -lm").exit(0)
