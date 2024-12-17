ecee3fe727b6064279016c1a9e78a50a8b49ad07
Bill Levien <blevien@users.noreply.github.com>
2024-12-17 00:21:15 +0000

    Update Seven

--- seven/__init__.py
+++ seven/__init__.py
@@ -2,5 +2,5 @@ import check50
 
 @check50.check()
 def exists():
-    """seven.py exists"""
-    check50.exists("seven.py")
+    """seven-day-average.py exists"""
+    check50.exists("seven-day-average.py")
